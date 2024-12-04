5394ad6522c3ad85fb3632f16533d67db73816bb
Tomáš Pacák <pacak.tomik@gmail.com>
2024-12-04 15:58:18 +0100

    #17 hodina

---
+++ IS-projekty/program017a-Fibonacci/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+
+        string again = "a";
+        while (again=="a") {
+            Console.Clear();
+            Console.WriteLine("*************************************");
+            Console.WriteLine("***fibo***");
+            Console.WriteLine("  **********Tomáš Pacák***********");
+            Console.WriteLine("            **********");
+            Console.WriteLine();
+
+            //input
+            Console.WriteLine();
+            Console.Write("Zadejte počet členů: ");
+            int n;
+            while(!int.TryParse(Console.ReadLine(), out n)) {
+                Console.Write ("Nezadali jste celé číslo");
+            }
+            Console.WriteLine();
+
+            Console.Write("0 1");
+            //psl
+            int a0 = 0;
+            int a1 = 1;
+            int aa = 0;
+            int suma = 0;
+            for(int i=0; i <= n+1; i++) {
+                aa = a1 + a0;
+                a0 = a1;
+                a1 = aa;
+                suma += aa;
+                Console.Write($" {aa}");
+            }
+
+            Console.Write($"suma: {suma}");
+
+            
+            Console.WriteLine();
+            
+
+            //konec programu/pokracovani
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování stiskněte klávesu a");
+            again = Console.ReadLine();
+
+        } 
+    }
+}
